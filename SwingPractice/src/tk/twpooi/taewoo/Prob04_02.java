49998b3bb3cba6fd01361a2e63a1d89dcf208ef8
DESKTOP-34J38EK\tw <ldayou@naver.com>
2016-11-19 17:06:27 +0900

    기능 추가
    * 4-2 : 화면을 키우면 그려진 원도 같이 커지기

--- SwingPractice/src/tk/twpooi/taewoo/Prob04_02.java
+++ SwingPractice/src/tk/twpooi/taewoo/Prob04_02.java
@@ -6,6 +6,8 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,33 +16,36 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-public class Prob04 {
+public class Prob04_02 {
 
 	public static void main(String[] args) {
-		
+
 		JFrame f = new JFrame();
 
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setTitle("Prob04");
+		f.setTitle("Prob04_02");
 		f.setSize(500, 500);
-		
-		Prob04Panel panel = new Prob04Panel();
+
+		Prob04_02Panel panel = new Prob04_02Panel();
 		f.add(panel);
-		
+
 		f.setVisible(true);
 
 	}
 
 }
 
-class Prob04Panel extends JPanel implements MouseListener, MouseMotionListener{
+class Prob04_02Panel extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener{
 	
 	private JLabel numLabel;
 	
 	private ArrayList<HashMap<String, Object>> list;
 	private ArrayList<Integer> innerIndexList;
 	
-	Prob04Panel(){
+	boolean isDrawing;
+	int x1, x2, y1, y2;
+	
+	Prob04_02Panel(){
 		
 		this.list = new ArrayList<>();
 		this.innerIndexList = new ArrayList<>();
@@ -53,6 +58,21 @@ class Prob04Panel extends JPanel implements MouseListener, MouseMotionListener{
 		
 		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
+		this.addMouseWheelListener(this);
+		
+	}
+	
+	private void addCircle(int x, int y, int r){
+		
+		HashMap<String, Object> temp = new HashMap<>();
+		
+		
+		temp.put("x", (double)x/getSize().getWidth());
+		temp.put("y", (double)y/getSize().getHeight());
+		temp.put("color", Color.red);
+		temp.put("r", (double)r/getSize().getWidth());
+		
+		list.add(temp);
 		
 	}
 	
@@ -60,23 +80,30 @@ class Prob04Panel extends JPanel implements MouseListener, MouseMotionListener{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		
+		
 		for(HashMap<String, Object> hash : list){
 			
-			int x1 = (int)hash.get("x1");
-			int y1 = (int)hash.get("y1");
-			int x2 = (int)hash.get("x2");
-			int y2 = (int)hash.get("y2");
+			int x = (int) ((double)hash.get("x")*getSize().getWidth());
+			int y = (int) ((double)hash.get("y")*getSize().getHeight());
 			Color color = (Color)hash.get("color");
 
-			int r = (int)Math.sqrt(Math.pow((double)x1-x2, 2) + Math.pow((double)y1-y2, 2));
-			
-			int x = x1-r;
-			int y = y1-r;
+			int r = (int) ((double)hash.get("r")*getSize().getWidth());
 			
 			g.setColor(color);
-			g.fillOval(x, y, r*2, r*2);
+			g.fillOval(x-r, y-r, r*2, r*2);
 			g.setColor(Color.black);
-			g.drawOval(x, y, r*2, r*2);
+			g.drawOval(x-r, y-r, r*2, r*2);
+			
+		}
+		
+		if(isDrawing){
+
+			int r = (int) Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+			
+			g.setColor(Color.red);
+			g.fillOval(x1-r, y1-r, r*2, r*2);
+			g.setColor(Color.black);
+			g.drawOval(x1-r, y1-r, r*2, r*2);
 			
 		}
 		
@@ -86,41 +113,37 @@ class Prob04Panel extends JPanel implements MouseListener, MouseMotionListener{
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
 		if(innerIndexList.size() == 0){ // 원이 선택되어진 상황이 아니라면
-			HashMap<String, Object> temp = list.get(list.size()-1);
-			temp.put("x2", e.getX());
-			temp.put("y2", e.getY());
-			list.set(list.size()-1, temp);
+			
+			x2 = e.getX();
+			y2 = e.getY();
+			
 			repaint();
+			
 		}else{
-			int ex = e.getX();
-			int ey = e.getY();
+			double ex = (double)e.getX()/getSize().getWidth();
+			double ey = (double)e.getY()/getSize().getHeight();
 			
 			for(Integer index : innerIndexList){
 				
 				HashMap<String, Object> temp = list.get(index);
+
+				double x = (double)temp.get("x");
+				double y = (double)temp.get("y");
 				
-				int x1 = (int)temp.get("x1");
-				int y1 = (int)temp.get("y1");
-				int x2 = (int)temp.get("x2");
-				int y2 = (int)temp.get("y2");
+				double x3 = (double)temp.get("x3");
+				double y3 = (double)temp.get("y3");
 				
-				int x3 = (int)temp.get("x3");
-				int y3 = (int)temp.get("y3");
+				double rx = ex - x3;
+				double ry = ey - y3;
 				
-				int rx = ex - x3;
-				int ry = ey - y3;
-				
-				x1 += rx;
-				x2 += rx;
+				x += rx;
 				x3 += rx;
-				y1 += ry;
-				y2 += ry;
+				
+				y += ry;
 				y3 += ry;
 				
-				temp.put("x1", x1);
-				temp.put("y1", y1);
-				temp.put("x2", x2);
-				temp.put("y2", y2);
+				temp.put("x", x);
+				temp.put("y", y);
 				temp.put("x3", x3);
 				temp.put("y3", y3);
 				
@@ -142,29 +165,25 @@ class Prob04Panel extends JPanel implements MouseListener, MouseMotionListener{
 		for(int i=0; i<list.size(); i++){ // 마우스를 클릭한 위치에 원이 이미 그려져 있는지 확인
 			HashMap<String, Object> temp = list.get(i);
 			
-			int x1 = (int)temp.get("x1");
-			int y1 = (int)temp.get("y1");
-			int x2 = (int)temp.get("x2");
-			int y2 = (int)temp.get("y2");
+			int x = (int) ((double)temp.get("x")*getSize().getWidth());
+			int y = (int) ((double)temp.get("y")*getSize().getHeight());
+
+			int r = (int) ((double)temp.get("r")*getSize().getWidth());
 			
-			int r = (int)Math.sqrt(Math.pow((double)x1-x2, 2) + Math.pow((double)y1-y2, 2));
-			
-			if((int)Math.sqrt(Math.pow((double)x1-e.getX(), 2) + Math.pow((double)y1-e.getY(), 2)) <= r){
+			if((int)Math.sqrt(Math.pow((double)x-e.getX(), 2) + Math.pow((double)y-e.getY(), 2)) <= r){
 				innerIndexList.add(i);
 			}
 			
 		}
 		
 		if(innerIndexList.size() == 0){ // 이미 그려진 원이 없다면 새로운 원을 추가
-			HashMap<String, Object> temp = new HashMap<>();
-			temp.put("x1", e.getX());
-			temp.put("y1", e.getY());
-			temp.put("x2", e.getX());
-			temp.put("y2", e.getY());
-			temp.put("color", Color.RED);
-			list.add(temp);
-			numLabel.setText("Num Of Rects : " + list.size());
+			
+			isDrawing = true;
+			x1 = x2 = e.getX();
+			y1 = y2 = e.getY();
+			
 			repaint();
+			
 		}else{ // 이미 그려진 원이 있다면
 			
 			int ex = e.getX();
@@ -174,8 +193,8 @@ class Prob04Panel extends JPanel implements MouseListener, MouseMotionListener{
 				
 				HashMap<String, Object> temp = list.get(index);
 				
-				temp.put("x3", ex);
-				temp.put("y3", ey);
+				temp.put("x3", (double)ex/getSize().getWidth());
+				temp.put("y3", (double)ey/getSize().getHeight());
 				
 				temp.put("color", Color.yellow);
 				
@@ -191,11 +210,52 @@ class Prob04Panel extends JPanel implements MouseListener, MouseMotionListener{
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		for(int index : innerIndexList){
-			list.get(index).put("color", Color.red);
+		
+		if(isDrawing){
+
+			int r = (int) Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+			
+			addCircle(x1, y1, r);
+			
+			isDrawing = false;
+			
+		}else{
+
+			for(int index : innerIndexList){
+				list.get(index).put("color", Color.red);
+			}
+			innerIndexList.clear();
+			
 		}
-		innerIndexList.clear();
+		
+		numLabel.setText("Num Of Circles : " + list.size());
+		
 		repaint();
+		
+	}
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		
+		for(int i : innerIndexList){
+
+			HashMap<String, Object> temp = list.get(i);
+
+			int r = (int) ((double)temp.get("r")*getSize().getWidth());
+			r += (e.getWheelRotation() * -2);
+			
+			if(r<5){
+				r = 5;
+			}
+			
+			temp.put("r", (double)r/getSize().getWidth());
+
+			list.set(i, temp);
+
+		}
+
+		repaint();
+
 		
 	}
 
